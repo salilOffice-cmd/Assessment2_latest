1c97ddfeadbd3e7c9151f96be924125e898ea444
SalilDeo_Office <SalilDeo_Office>
2023-11-24 13:37:52 +0530

    Initial commit

---
+++ WebApi/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using Application.CoursesCQRS.Commands;
+using Application.CoursesCQRS.Queries;
+using Application.DTOs.CourseDTOs;
+using Application.StudentsCQRS.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class CourseController : APIControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult> AddCourseAsync([FromBody] AddCourseDTO _addCourseDTO)
+        {
+            var addCourseCommand = new AddCourseCommand { addCourseDTO = _addCourseDTO};
+            var gotAddedCourse = await Mediator.Send(addCourseCommand);
+            return Ok(gotAddedCourse);
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<ActionResult> GetAllCoursesAsync()
+        {
+            var gotAllCourses = await Mediator.Send(new GetAllCoursesQuery { });
+            return Ok(gotAllCourses);
+        }
+
+    }
+}
---
+++ WebApi/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using Application.CoursesCQRS.Commands;
+using Application.DTOs.CourseDTOs;
+using Application.DTOs.StudentDTOs;
+using Application.StudentsCQRS.Commands;
+using Application.StudentsCQRS.Queries;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class StudentController : APIControllerBase
+    {
+        [HttpPost]
+        [Route("Add")]
+        public async Task<ActionResult> AddStudentAsync([FromBody] AddStudentDTO _addStudentDTO)
+        {
+            var addStudentCommand = new AddStudentCommand { addStudentDTO = _addStudentDTO };
+            var gotAddedStudent = await Mediator.Send(addStudentCommand);
+            return Ok(gotAddedStudent);
+        }
+
+        [HttpGet]
+        [Route("getAll")]
+        public async Task<ActionResult> GetAllStudentsAsync()
+        {
+            var gotAllStudents = await Mediator.Send(new GetAllStudentsQuery { });
+            return Ok(gotAllStudents);
+        }
+    }
+}
